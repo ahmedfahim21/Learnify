3dd2ae94ccd87c5b9baf2166f64897098453f97c
Mohd Dilshad <mohddilshad.211it042@nitk.edu.in>
2023-10-15 00:49:23 +0530

    client: separated chatbot

--- client/src/Components/Article.jsx
+++ client/src/Components/Article.jsx
@@ -1,46 +1,22 @@
 import React, { useState } from 'react';
 import Backbtn from './Buttons/Backbtn';
+import ChatBot from './chatBot';
+import MainBox from './chatBot2';
+import axios from 'axios';
 
 function Article() {
-  const [messages, setMessages] = useState([
-    { text: 'Hello!', sender: 'user' },
-    { text: 'Hi there!', sender: 'bot' },
-  ]);
-
-  const [newMessage, setNewMessage] = useState('');
-  // const messagesContainerRef = useRef(null);
-
-  const handleSendMessage = () => {
-    if (newMessage.trim() !== '') {
-      setMessages((prevMessages) => [...prevMessages, { text: newMessage, sender: 'user' }]);
-      setNewMessage('');
-      // Simulate a bot response (you can replace this with actual bot logic)
-      setTimeout(() => {
-        setMessages((prevMessages) => [...prevMessages, { text: 'I am a bot.', sender: 'bot' }]);
-      }, 2000);
-    }
-  };
 
 
-  const fetchResponse = async () => {
-    // const response = await fetch('https://api.github.com/users');
-    // const data = await response.json();
+  const article = "The French Revolution, a seminal event in late 18th-century France, marked a watershed moment in the history of both the nation and the modern world. Emerging in 1789, it was driven by a complex web of social, political, and economic factors. The revolution began with a groundswell of discontent among the common people, who were suffering under heavy taxation, while the nobility and clergy enjoyed privileges and tax exemptions. This discontent culminated in the famous storming of the Bastille in July 1789, which symbolized the people's revolt against oppression. As the revolution unfolded, it went through various phases, including the Reign of Terror led by Maximilien Robespierre, which saw mass executions and radical political changes.";
 
 
-    // console.log(data);
-  };
 
-  const handleSend = () => {
-    handleSendMessage();
-    fetchResponse();
-  }
 
-  // useEffect(() => {
-  //   // Automatically scroll to the end of the chat container
-  //   if (messagesContainerRef.current) {
-  //     messagesContainerRef.current.scrollTop = messagesContainerRef.current.scrollHeight;
-  //   }
-  // }, [messages]);
+
+  
+  
+
+
 
   return (
     <div className='min-h-screen'>
@@ -64,41 +40,15 @@ function Article() {
       </div>
 
 
-      <div className="w-1/4 border border-gray-300 rounded-lg min-h-96 h-auto mr-5">
-        <div className="bg-gradient-to-r from-green-400 to-blue-500 py-2 text-white text-center font-semibold rounded-t-lg">
-          AI Tutor
-        </div>
-        {/* Chat UI */}
-        <div className="bg-white p-4 h-full flex flex-col justify-between">
-          <div className="flex-1 overflow-y-auto">
-            {messages.map((message, index) => (
-              <div
-                key={index}
-                className={message.sender == 'user' ? 'text-right mb-2' : 'text-left mb-2'}
-              >
-                <span className={message.sender === 'user' ? 'bg-blue-500 text-white rounded-md p-2 inline-block max-w-md' : 'bg-gray-500 text-white rounded-md p-2 inline-block max-w-md'}>
-                  {message.text}
-                </span>
-              </div>
-            ))}
-          </div>
-          <div className="flex">
-            <input
-              type="text"
-              value={newMessage}
-              onChange={(e) => setNewMessage(e.target.value)}
-              placeholder="Type a message..."
-              className="flex-1 border p-2 rounded-l-md"
-            />
-            <button
-              onClick={handleSendMessage}
-              className="bg-blue-500 text-white rounded-r-md p-2 hover:bg-blue-700"
-            >
-              Send
-            </button>
-          </div>
-        </div>
-      </div>
+      
+    {/* <ChatBot article={article}/> */}
+
+    <MainBox article={article}/>
+
+
+
+
+
     </div>
     </div>
 
