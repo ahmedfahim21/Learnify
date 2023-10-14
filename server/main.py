9458a00a895b4e78e007f95777800ac0d5d1c395
Mohd Dilshad <mohddilshad.211it042@nitk.edu.in>
2023-10-14 16:10:56 +0530

    backend: integrated flow charts

--- server/main.py
+++ server/main.py
@@ -9,6 +9,7 @@ from getTopics import get_Modules
 from getArticle import get_Article
 from getQuiz import get_Quiz
 from chatBot import chatBot
+from get_flowchart import get_flowchart
 
 # Register the blueprints
 app.register_blueprint(getGlimpse)
@@ -17,6 +18,7 @@ app.register_blueprint(get_Modules)
 app.register_blueprint(get_Article)
 app.register_blueprint(get_Quiz)
 app.register_blueprint(chatBot)
+app.register_blueprint(get_flowchart)
 
 # @app.route('/', methods=['GET'])
 # def test():
