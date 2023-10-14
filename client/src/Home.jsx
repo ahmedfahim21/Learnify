1d8083ba17a56f1335c74d4228a3ea1da5957c04
Chirag Rajput <103127634+Chirag2203@users.noreply.github.com>
2023-10-14 05:50:22 +0000

    Client: add router

---
+++ client/src/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from './Components/Hero'
+import About from './Components/About'
+function Home() {
+  return (
+    <div>
+        <Hero />
+        <About />
+
+    </div>
+  )
+}
+
+export default Home
