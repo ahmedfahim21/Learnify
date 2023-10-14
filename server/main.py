655030e880134a850208a994b81040ed656ae635
Mohd Dilshad <mohddilshad.211it042@nitk.edu.in>
2023-10-14 15:36:27 +0530

    backend: added other routes

--- server/main.py
+++ server/main.py
@@ -4,15 +4,19 @@ app = Flask(__name__)
 
 # Import the routes for GET and POST requests
 from getGlimpse import getGlimpse
-from server2 import post_routes
+# from server2 import post_routes
 from getTopics import get_Modules
 from getArticle import get_Article
+from getQuiz import get_Quiz
+from chatBot import chatBot
 
 # Register the blueprints
 app.register_blueprint(getGlimpse)
-app.register_blueprint(post_routes)
+# app.register_blueprint(post_routes)
 app.register_blueprint(get_Modules)
 app.register_blueprint(get_Article)
+app.register_blueprint(get_Quiz)
+app.register_blueprint(chatBot)
 
 # @app.route('/', methods=['GET'])
 # def test():
