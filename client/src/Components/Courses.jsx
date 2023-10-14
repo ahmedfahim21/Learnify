6c5cb137394f3e5a2844a4015fa71ef99b116660
ahmedfahim21 <ahmed.fahim0207@gmail.com>
2023-10-14 19:55:09 +0530

    Client: first post req

--- client/src/Components/Courses.jsx
+++ client/src/Components/Courses.jsx
@@ -2,26 +2,46 @@ import { useEffect, useState } from 'react'
 import { collection, addDoc, onSnapshot, query} from "firebase/firestore"; 
 import { db } from '../firebase'
 import CourseCard from './Cards/CourseCard'
+import axios from 'axios'
 
 
 function Courses() {
+
+    const apiUrl = 'http://localhost:5000';
     const [course, setChapters] = useState([])
 
 
     function CourseAdd() {
-        const [newChapter, setNewChapter] = useState({ name: '', content: '', media: '' })
+        const [newChapter, setNewChapter] = useState({ name: ''})
 
         //add
         const addChapter = async (e) => {
             e.preventDefault()
             if (newChapter.name!=''){
-    
-            await addDoc(collection(db, "Course"), {
-                name: newChapter.name,
-                content: newChapter.content,
-                media: newChapter.media
-            });
-            setNewChapter({ name: '', content: '', media: '' })
+
+            const postData = {
+                userInput: `${newChapter.name}`
+            };
+
+            console.log(postData)
+
+            try {
+                // Make an asynchronous POST request
+                const res = await axios.post(`${apiUrl}/get_ShortNote`, postData);
+                console.log(res)
+
+                await addDoc(collection(db, "Course"), {
+                    name: newChapter.name,
+                    content: res.data,
+                });
+                setNewChapter({ name: '' })
+
+                
+              } catch (error) {
+                // Handle errors
+                console.error('POST request error:', error);
+              }
+
         }
         }
 
@@ -33,7 +53,7 @@ function Courses() {
             <div className="mt-4">
             <form className="flex flex-col">
                 <input type="text" value={newChapter.name} onChange={(e)=>{ setNewChapter({...newChapter, name: e.target.value})}} placeholder="Enter Course name" className="flex px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-200 focus:border-transparent" /> 
-                <input type="text" value={newChapter.content} onChange={(e)=>{ setNewChapter({...newChapter, content: e.target.value})}} placeholder="Enter content" className="flex px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-200 focus:border-transparent" />
+                {/* <input type="text" value={newChapter.content} onChange={(e)=>{ setNewChapter({...newChapter, content: e.target.value})}} placeholder="Enter content" className="flex px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-200 focus:border-transparent" /> */}
                 {/* <input type="text" value={newChapter.media} onChange={(e)=>{ setNewChapter({...newChapter, media: e.target.value})}} placeholder="Enter media" className="flex px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-200 focus:border-transparent" /> */}
                 <button onClick={addChapter} className="flex px-4 py-2 mt-4 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">Add</button>
               </form>
